6a467b2b09b029206bbd4415f58193676c644dc7
Hadar <hadar5445@gmail.com>
2025-01-18 15:04:50 +0200

    chekbox in view holder update data base

--- app/src/main/java/com/example/studentslistapp/adapter/StudentViewHolder.kt
+++ app/src/main/java/com/example/studentslistapp/adapter/StudentViewHolder.kt
@@ -4,7 +4,8 @@ import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import com.example.studentslistapp.databinding.ItemStudentBinding
 
-class StudentViewHolder(private val binding: ItemStudentBinding, listener: OnItemClickListener?) : RecyclerView.ViewHolder(binding.root) {
+class StudentViewHolder(private val binding: ItemStudentBinding, listener: OnItemClickListener?) :
+    RecyclerView.ViewHolder(binding.root) {
 
     private var student: Student? = null
 
@@ -13,6 +14,25 @@ class StudentViewHolder(private val binding: ItemStudentBinding, listener: OnIte
         binding.root.setOnClickListener {
             Log.d("TAG", "On click listener on position $adapterPosition")
             listener?.onItemClick(student)  // Call onItemClick when an item is clicked
+        }
+
+        binding.checkbox.setOnCheckedChangeListener { buttonView, isChecked ->
+            student?.let {
+                val newStudent = student?.let {
+                    Student(
+                        it.id,
+                        it.name,
+                        it.phone,
+                        it.address,
+                        isChecked
+                    )
+                }
+                if (newStudent != null) {
+                    Model.shared.editStudent(newStudent, it.id) {
+                    }
+                }
+            }
+
         }
     }
 
